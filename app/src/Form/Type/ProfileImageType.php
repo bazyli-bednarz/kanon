55575c8b6ce20e2d6df561864ff5051ddccaf351
Bazyli <bazyli.bednarz@gmail.com>
2022-05-15 14:15:04 +0200

    Added experience requirements for profile images

--- app/src/Form/Type/ProfileImageType.php
+++ app/src/Form/Type/ProfileImageType.php
@@ -9,24 +9,58 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 
 class ProfileImageType extends AbstractType
 {
+    private Security $security;
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('image',
             ChoiceType::class,
                 [
-                    'choices' => [
-                        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-                    ],
+                    'choices' => $this->determineChoices(),
                     'expanded' => true,
                     'mapped' => false,
                     'label' => 'label.profile_image',
+                    'required' => true,
                 ]
             )
         ;
+    }
+
+    public function determineChoices(): array {
+        $level = $this->security->getUser()->getLevel();
+        $choices = [1,2,3,4,5];
+        switch ($level) {
+            case 1:
+                return $choices;
+            case 2:
+                return array_merge($choices, [6,7,8]);
+            case 3:
+                return array_merge($choices, [6,7,8,9,10]);
+            case 4:
+                return array_merge($choices, [6,7,8,9,10,11,12]);
+            case 5:
+                return array_merge($choices, [6,7,8,9,10,11,12,13,14]);
+            case 6:
+                return array_merge($choices, [6,7,8,9,10,11,12,13,14,15,16]);
+            case 7:
+                return array_merge($choices, [6,7,8,9,10,11,12,13,14,15,16,17]);
+            case 8:
+                return array_merge($choices, [6,7,8,9,10,11,12,13,14,15,16,17,18]);
+            case 9:
+                return array_merge($choices, [6,7,8,9,10,11,12,13,14,15,16,17,18,19]);
+            default:
+                return array_merge($choices, [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+        }
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
